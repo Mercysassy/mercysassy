005c5d04ec4bf6df5e8ba24b3de7ddc65befdf25
Mercysassy <Mercysassy>
2025-02-19 03:17:27 -0800

    mercysassy

---
+++ database/migrations/2025_02_19_104021_create_blogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->string('images')->nullable;
+            $table->string('slug')->nullable;
+            $table->string('status')->nullable;
+            $table->string('category')->nullable;
+            $table->string('author')->nullable;
+            $table->string('ref_no')->nullable;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
