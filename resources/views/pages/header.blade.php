8ebbac340590288c54893e2c141a1f28d2067f56
Mercysassy <Mercysassy>
2025-02-21 04:23:56 -0800

    welcome about header home  footer

---
+++ resources/views/pages/header.blade.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Mirrored from themexriver.com/tfhtml/finano/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 17 Jan 2019 01:59:48 GMT -->
+<head>
+<meta charset="utf-8">
+<title> MERCYSASSY | Homepage Two</title>
+<!-- Stylesheets -->
+<link href="{{ asset('front/css/bootstrap.css')}}" rel="stylesheet">
+<link href="{{ asset('front/css/style.css')}}" rel="stylesheet">
+<link href="{{ asset('front/css/responsive.css')}}" rel="stylesheet">
+
+<link rel="shortcut icon" href="{{ asset('front/images/favicon.png')}}" type="image/x-icon">
+<link rel="icon" href="{{ asset('front/images/favicon.png')}}" type="image/x-icon">
+
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+
+<div class="page-wrapper">
+ 	
+    <!-- Preloader -->
+    <div class="preloader"></div>
+ 	
+    <!-- Main Header-->
+    <header class="main-header header-style-two">
+    	
+        <!--Header-Upper-->
+        <div class="header-upper">
+        	<div class="auto-container">
+            	<div class="upper-inner clearfix">
+                	
+                	<div class="pull-left logo-box">
+                    	<div class="logo"><a href="index.html"><img src="images/logo-2.png" alt="" title=""></a></div>
+                    </div>
+                    
+					<div class="upper-right clearfix">
+                    	
+                        <!--Info Box-->
+                        <div class="upper-column info-box">
+                        	<div class="icon-box"><span class="flaticon-e-mail-envelope"></span></div>
+                            <ul>
+                            	<li>mercy@mail.com</li>
+                            </ul>
+                        </div>
+                        
+                        <!--Info Box-->
+                        <div class="upper-column info-box">
+                        	<div class="icon-box"><span class="flaticon-phone-receiver"></span></div>
+                            <ul>
+								<li>+234 801 572 4423</li>
+                            </ul>
+                        </div>
+						
+						<!--Info Box-->
+                        <div class="upper-column info-box">
+                        	<div class="icon-box"><span class="fa fa-map-marker"></span></div>
+                            <ul>
+								<li>121 King Street, Uyo, Nigeria</li>
+</ul>
+                        </div>
+                        
+						<div class="social-box">
+							<ul class="social-icon-one">
+								<li><a href="#"><span class="fa fa-facebook-square"></span></a></li>
+								<li><a href="#"><span class="fa fa-twitter-square"></span></a></li>
+								<li><a href="#"><span class="fa fa-linkedin-square"></span></a></li>
+								<li><a href="#"><span class="fa fa-google-plus-square"></span></a></li>
+								<li><a href="#"><span class="fa fa-rss-square"></span></a></li>
+							</ul>
+						</div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <!--End Header Upper-->
+
+		<!-- Header Lower  -->
+    	<div class="header-lower">
+        	<div class="auto-container">
+            	<div class="outer-container clearfix">
+                    <div class="nav-outer clearfix">
+                    
+						<!-- Main Menu -->
+						<nav class="main-menu navbar-expand-md">
+							<div class="navbar-header">
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+
+							<div class="navbar-collapse collapse clearfix" id="navbarSupportedContent">
+								<ul class="navigation clearfix">
+								<li><a href="{{ url('/') }}">Home</a></li>
+								<li><a href="{{ url('about') }}">About us</a></li>
+
+										
+									<li class="dropdown"><a href="#">Services</a>
+										<ul>
+											<li><a href="services.html">Services</a></li>
+											<li><a href="services-2.html">Services Two</a></li>
+											<li><a href="services-single.html">Services Single</a></li>
+										</ul>
+									</li>
+									<li class="dropdown"><a href="#">Portfolio</a>
+										<ul>
+											<li><a href="portfolio-2-column.html">Portfolio Two Column</a></li>
+											<li><a href="portfolio-3-column.html">Portfolio Three Column</a></li>
+											<li><a href="portfolio-4-column.html">Portfolio Four Column</a></li>
+											<li><a href="portfolio-masonry.html">Portfolio Masonry</a></li>
+											<li><a href="portfolio-single.html">Portfolio Single</a></li>
+										</ul>
+									</li>
+									<li class="dropdown"><a href="#">Blog</a>
+										<ul>
+											<li><a href="blog.html">Our Blog</a></li>
+											<li><a href="blog-list.html">Blog List</a></li>
+											<li><a href="blog-single.html">Blog Single</a></li>
+										</ul>
+									</li>
+									<li><a href="contact.html">Contact us</a></li>
+								</ul>
+							</div>
+							
+						</nav>
+                    </div>
+					
+					<!--Outer Box-->
+					<div class="outer-box">
+						
+						<!--Cart Box-->
+						<div class="cart-box">
+							<div class="cart-box-btn dropdown-toggle"><span class="fa fa-shopping-cart"></span><span class="total-cart">0</span></div>
+						</div>	
+						
+						<!--Search Box-->
+						<div class="search-box-outer">
+							<div class="dropdown">
+								<button class="search-box-btn dropdown-toggle" type="button" id="dropdownMenu3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fa fa-search"></span></button>
+								<ul class="dropdown-menu pull-right search-panel" aria-labelledby="dropdownMenu3">
+									<li class="panel-outer">
+										<div class="form-container">
+											<form method="post" action="http://themexriver.com/tfhtml/finano/blog.html">
+												<div class="form-group">
+													<input type="search" name="field-name" value="" placeholder="Search Here" required>
+													<button type="submit" class="search-btn"><span class="fa fa-search"></span></button>
+												</div>
+											</form>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+						
+						<!--Language-->
+						<div class="language dropdown"><a class="btn btn-default dropdown-toggle" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" href="#"><span class="flag-icon"><img src="images/icons/flag-icon.png" alt=""/></span>En</a>
+							<ul class="dropdown-menu style-one" aria-labelledby="dropdownMenu2">
+								<li><a href="#">English</a></li>
+								<li><a href="#">German</a></li>
+								<li><a href="#">Arabic</a></li>
+								<li><a href="#">Hindi</a></li>
+							</ul>
+						</div>
+						
+					</div>
+					
+                </div>
+            </div>
+        </div>
+		
+		<!--Sticky Header-->
+        <div class="sticky-header">
+        	<div class="auto-container clearfix">
+            	<!--Logo-->
+            	<div class="logo pull-left">
+                	<a href="index.html" class="img-responsive"><img src="images/logo-small.png" alt="" title=""></a>
+                </div>
+                
+                <!--Right Col-->
+                <div class="right-col pull-right">
+                	<!-- Main Menu -->
+                    <nav class="main-menu navbar-expand-md">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        
+                        <div class="navbar-collapse collapse clearfix" id="navbarSupportedContent1">
+                            <ul class="navigation clearfix">
+
+							<li><a href="contact.html">Contact us</a></li>
+
+							<!-- <li><a href="{{url('/')}}">Home</a></li> -->
+
+								<li class="dropdown"><a href="#">About Us</a>
+									<ul>
+										<li><a href="about.html">About Us</a></li>
+										<li><a href="team.html">Our Team</a></li>
+										<li><a href="case-studies.html">Case Studies</a></li>
+										<li><a href="testimonial.html">Testimonial</a></li>
+										<li><a href="not-found.html">Error Page</a></li>
+									</ul>
+								</li>
+								<li class="dropdown"><a href="#">Services</a>
+									<ul>
+										<li><a href="services.html">Services</a></li>
+										<li><a href="services-2.html">Services Two</a></li>
+										<li><a href="services-single.html">Services Single</a></li>
+									</ul>
+								</li>
+								<li class="dropdown"><a href="#">Portfolio</a>
+									<ul>
+										<li><a href="portfolio-2-column.html">Portfolio Two Column</a></li>
+										<li><a href="portfolio-3-column.html">Portfolio Three Column</a></li>
+										<li><a href="portfolio-4-column.html">Portfolio Four Column</a></li>
+										<li><a href="portfolio-masonry.html">Portfolio Masonry</a></li>
+										<li><a href="portfolio-single.html">Portfolio Single</a></li>
+									</ul>
+								</li>
+								<li class="dropdown"><a href="#">Blog</a>
+									<ul>
+										<li><a href="blog.html">Our Blog</a></li>
+										<li><a href="blog-list.html">Blog List</a></li>
+										<li><a href="blog-single.html">Blog Single</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contact us</a></li>
+                            </ul>
+                        </div>
+                    </nav><!-- Main Menu End-->
+                </div>
+                
+            </div>
+        </div>
+        <!--End Sticky Header-->
+		
+    </header>
+    <!--End Main Header -->
+	
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('about', function () {
+    return view('pages.about');
+});
+Route::get('services', function () {
+    return view('pages.services');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
